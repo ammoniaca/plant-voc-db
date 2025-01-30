106b33e27c36bd34e578c6ed57f7a4a96d614b4c
Alessandro <alessandro.montaghi@gmail.com>
2025-01-30 17:27:53 +0100

    added missing @Repository annotation to PlantVocRepository Interface

--- plantvocdb/src/main/java/org/cnr/plantvocdb/repository/PlantVocRepository.java
+++ plantvocdb/src/main/java/org/cnr/plantvocdb/repository/PlantVocRepository.java
@@ -2,9 +2,11 @@ package org.cnr.plantvocdb.repository;
 
 import org.cnr.plantvocdb.entity.PlantVocEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface PlantVocRepository
         extends JpaRepository<PlantVocEntity, UUID> {
 }
