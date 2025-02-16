3688e8e3cf24adfc0d3e80b56c3e7d7f43001fa6
Alessandro <alessandro.montaghi@gmail.com>
2025-02-16 22:27:48 +0100

    refactoring GlobalExceptionHandler

--- plantvocdb/src/main/java/org/cnr/plantvocdb/exceptions/GlobalExceptionHandler.java
+++ plantvocdb/src/main/java/org/cnr/plantvocdb/exceptions/GlobalExceptionHandler.java
@@ -1,27 +1,58 @@
 package org.cnr.plantvocdb.exceptions;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(PlantNotFoundException.class)
-    public ResponseEntity<?> handlePlantNotFoundException(PlantNotFoundException ex) {
-      ErrorResponseDTO plantNotFound = ErrorResponseDTO
-              .builder().timestamp(LocalDateTime.now())
+    public ResponseEntity<?> handlePlantNotFoundException(
+            PlantNotFoundException exception,
+            WebRequest request
+    ) {
+        String path = request
+                .getDescription(false)
+                .replace("uri=", "");
+
+        ErrorResponseDTO plantNotFound = ErrorResponseDTO
+              .builder()
+              .timestamp(LocalDateTime.now())
               .status(HttpStatus.NOT_FOUND.value())
-              .details(ex.getMessage())
-              .message(HttpStatus.NOT_FOUND.name())
+              .details(exception.getMessage())
+              .message(HttpStatus.NOT_FOUND.name()).instance(path)
               .build();
 
       return ResponseEntity
               .status(HttpStatus.NOT_FOUND)
               .body(plantNotFound);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponseDTO> handleValidationException(HttpMessageNotReadableException exception) {
+        String errorDetails = "";
+
+        if (exception.getCause() instanceof InvalidFormatException) {
+            InvalidFormatException ifx = (InvalidFormatException) exception.getCause();
+            if (ifx.getTargetType()!=null && ifx.getTargetType().isEnum()) {
+                errorDetails = String.format("Invalid enum value: '%s' for the field: '%s'. The value must be one of: %s.",
+                        ifx.getValue(), ifx.getPath().get(ifx.getPath().size()-1).getFieldName(), Arrays.toString(ifx.getTargetType().getEnumConstants()));
+            }
+        }
+        ErrorResponseDTO errorResponse = ErrorResponseDTO
+                .builder()
+                .timestamp(LocalDateTime.now())
+                .details(errorDetails)
+                .build();
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
 }
